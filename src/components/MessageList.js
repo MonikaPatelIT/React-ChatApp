aaad80f5dae3e48d2feff6d4f888b5eb9d577466
Monika Patel <monikapatel.it@outlook.com>
2018-10-02 15:58:09 +1300

    Add style to the components

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
 
+//Dummy data to start work with
+const DUMMY_DATA = [
+    {
+        senderId: 'perborgen',
+        text: 'Hey, how is it going?'
+    },
+    {
+        senderId: 'janedoe',
+        text: 'Great! How about you?'
+    },
+    {
+        senderId: 'perborgen',
+        text: 'Good to hear! I am great as well'
+    }
+]
+
 class MessageList extends Component{
 render(){
 	return (
 			<div className="message-list">
-                <div className="help-text">MessageList</div>
+			{
+				DUMMY_DATA.map((message, index) => {
+				return ( <div key={index} className="message">
+                            <div className="message-username">{message.senderId}</div>
+                            <div className="message-text">{message.text}</div>
+                        </div>);
+				})
+			}				
             </div>
 			);
 }
